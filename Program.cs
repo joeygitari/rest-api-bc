14fa955679add52e6e79f3968d39d2e15af08184
joeygitari <gitarijoanne@gmail.com>
2024-11-07 10:37:01 +0300

    Get customer list and employee list

--- Program.cs
+++ Program.cs
@@ -4,21 +4,34 @@ using System.Net;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddHttpClient<TokenService>();
 builder.Services.AddScoped<TokenService>();
 builder.Services.AddScoped<CustomerService>();
 builder.Services.AddScoped<EmployeeService>();
-
 builder.Services.AddSingleton<TokenService>();
 builder.Services.AddMemoryCache();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// Register CustomerService with HttpClient and default credentials.
+builder.Services.AddHttpClient<CustomerService>()
+    .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+    {
+        UseDefaultCredentials = true,
+    });
+
+// Register EmployeeService with HttpClient and default credentials.
+builder.Services.AddHttpClient<EmployeeService>()
+    .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+    {
+        UseDefaultCredentials = true,
+    });
 
 var app = builder.Build();
 app.UseMiddleware<TokenAuthenticationMiddleware>();
+
 app.UseRouting();
 app.UseEndpoints(endpoints =>
 {
